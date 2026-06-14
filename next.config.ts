b4d90d2c65a7ba4bce5bea5252c2e9ba03092b43
sarahnandi <nandisarah20@gmail.com>
2026-06-14 16:36:27 +0530

    config: ignore typescript build errors for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   distDir: ".next-local",
+  typescript: {
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
   /* config options here */
 };
 
