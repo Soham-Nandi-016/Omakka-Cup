95acfb5d9fdf35e7f08805cfb0de7bddfa03cc6f
sarahnandi <nandisarah20@gmail.com>
2026-06-14 19:07:23 +0530

    fix: nest connection url under datasource object

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -7,7 +7,9 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
+    datasource: {
+      url: process.env.DATABASE_URL,
+    },
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
