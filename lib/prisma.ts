4669f9abf334305b7a62f7937d31b10444039e7e
sarahnandi <nandisarah20@gmail.com>
2026-06-15 18:54:33 +0530

    fix: manual production database inline literal assignment

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,6 +4,8 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+process.env.DATABASE_URL = "postgresql://postgres:OssOmakkaCup2026!@db.hksgtclthnehcoqkplpb.supabase.co:5432/postgres";
+
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
